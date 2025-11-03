d0564a8024375b2e420811617ab57024d27591dd
Thapelo Gaoboihe <thapelogaoboihe@gmail.com>
2025-11-03 17:22:58 +0200

    files added

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,6 @@
-num = int(input("Enter the size of pattern:"))
-for i in range(num):
+print("solid square")
+num = int(input("Enter the size of the pattern: "))
+for i in range(num ):
     for j in range(num):
-        print("*", end=" ")
-    print("")
-
+        print("*", end="")
+    print()
