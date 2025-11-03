a8b5ed0d97c5c36a002b5cc67c62ba73a7f78723
Thapelo Gaoboihe <thapelogaoboihe@gmail.com>
2025-11-03 14:45:19 +0200

    file fixed

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number = ""
 def Table(number):
-    number = int(input("Enter a number:"))
+    number = input("Enter a number:")
     for i in range(1, 11):
         print(f"{number} x {i} = {number*i}")
 Table(number)
