e47ba24a5a1a5b174befeb4f3697a19f0a92d025
Thapelo Gaoboihe <thapelogaoboihe@gmail.com>
2025-11-04 15:10:36 +0200

    files fixed

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,7 +6,7 @@ def display_menu():
     print("4. Exit")
 
 def main():
-    shopping_list = input("Enter to item to add: ")
+    shopping_list = []
     while True:
         display_menu()
         choice = input("Enter item to add: ")
